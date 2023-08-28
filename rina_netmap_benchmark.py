5fe487b6eccc5abd8f2f88b041cc21da8923ce3a
Sergio Giménez <sergio.gimenez@i2cat.net>
2023-08-28 12:20:46 +0200

    Add invocation of pkt-gen tx and some refactor

--- rina_netmap_benchmark.py
+++ rina_netmap_benchmark.py
@@ -9,23 +9,23 @@ import logger
 log = logger.setup_logger(file_name='pkt_gen_benchmark.log')
 
 
-def run_pkt_gen(mode: str, interface: str, num_packets: Optional[int] = None):
-    import pdb
-    pdb.set_trace()
-    if mode not in ['tx', 'rx']:
-        raise ValueError("Mode must be either 'tx' or 'rx'")
-    elif mode == 'rx' and num_packets is None:
-        raise ValueError(
-            "Number of packets must be specified when running in rx mode")
-
-    cmd = ['sudo', 'pkt-gen', '-i', interface, '-f', mode]
-
-    if mode == 'rx':
-        cmd.extend(['-n', str(num_packets)])
-
+def run_pkt_gen_rx(interface: str, num_packets: int):
+    cmd = ['sudo', 'pkt-gen', '-i', interface,
+           '-f', 'rx', '-n', str(num_packets)]
     result = subprocess.run(cmd, stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE, text=True)
     return result.stdout
+
+
+def run_pkt_gen_tx(interface: str):
+    cmd = ['sudo', 'pkt-gen', '-i', interface, '-f', 'tx']
+    pkt_gen_tx_pid = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE, text=True)
+    return pkt_gen_tx_pid
+
+
+def kill_pkt_gen_tx(pkt_gen_tx_pid):
+    pkt_gen_tx_pid.kill()
 
 
 def parse_output(output):
@@ -39,21 +39,21 @@ def parse_output(output):
     return metrics
 
 
-def main():
-    total_experiment_iterations = 2  # Number of times to run pkt-gen
-    processed_pkts_per_iteration = 100
-    tx_interface = "vale118:01"
-    rx_interface = "vale116:01"
+def run_experiment(total_experiment_iterations: int, tx_interface: str, rx_interface: str,  processed_pkts_per_iteration: int):
     all_metrics = []
-
+    pkt_gen_tx_pid = run_pkt_gen_tx(tx_interface)
     for _ in range(total_experiment_iterations):
         log.info("Running iteration number {}".format(_))
-        output = run_pkt_gen(mode='rx', interface=rx_interface,
-                             num_packets=processed_pkts_per_iteration)
+        output = run_pkt_gen_rx(interface=rx_interface,
+                                num_packets=processed_pkts_per_iteration)
         metrics = parse_output(output)
         log.info(metrics)
         all_metrics.append(metrics)
+    kill_pkt_gen_tx(pkt_gen_tx_pid)
+    return all_metrics
 
+
+def dump_metrics_into_csv(all_metrics):
     csv_file = 'pkt_gen_metrics.csv'
 
     # Write the metrics to the CSV file
@@ -70,6 +70,10 @@ def main():
     average_batch_data = [float(metrics['average_batch'])
                           for metrics in all_metrics]
 
+    return packets_per_sec_data, throughput_data, average_batch_data
+
+
+def draw_plots_in_pdf(packets_per_sec_data, throughput_data, average_batch_data):
     # Create a figure with subplots
     fig, axes = plt.subplots(3, 1, figsize=(10, 15))
     axes[0].plot(packets_per_sec_data, marker='o')
@@ -84,6 +88,23 @@ def main():
     axes[2].set_title('Average Batch Size')
     plt.tight_layout()
     plt.savefig('pkt_gen_plots.pdf', format='pdf')
+
+
+def main():
+    total_experiment_iterations = 30  # Number of times to run pkt-gen
+    processed_pkts_per_iteration = 100
+    tx_interface = "vale113:01"
+    rx_interface = "vale116:01"
+
+    all_metrics = run_experiment(total_experiment_iterations=total_experiment_iterations,
+                                 tx_interface=tx_interface,
+                                 rx_interface=rx_interface,
+                                 processed_pkts_per_iteration=processed_pkts_per_iteration)
+
+    packets_per_sec_data, throughput_data, average_batch_data = dump_metrics_into_csv(
+        all_metrics)
+
+    draw_plots_in_pdf(packets_per_sec_data, throughput_data, average_batch_data)
 
 
 if __name__ == "__main__":
