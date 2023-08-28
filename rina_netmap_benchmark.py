d1c4dc4d7b854b0ff3c95948693620f8b549a316
Sergio Giménez <sergio.gimenez@i2cat.net>
2023-08-28 12:44:23 +0200

    Capture metrics of every iteration

--- rina_netmap_benchmark.py
+++ rina_netmap_benchmark.py
@@ -32,10 +32,12 @@ def kill_pkt_gen_tx(pkt_gen_tx_pid):
 
 def parse_output(output):
     metrics = {}
-    metrics['packets_per_sec'] = re.search(
-        r'Speed: (\d+\.\d+) Mpps', output).group(1)
-    metrics['throughput'] = re.search(
-        r'Bandwidth: (\d+\.\d+) Gbps', output).group(1)
+    speed_match = re.search(r'Speed: (\d+\.\d+) (\w+)pps', output)
+    throughput_match = re.search(r'Bandwidth: (\d+\.\d+) (\w+)', output)
+    metrics['packets_per_sec'] = speed_match.group(1)
+    metrics['speed_units'] = speed_match.group(2)
+    metrics['throughput'] = throughput_match.group(1)
+    metrics['throughput_units'] = throughput_match.group(2)
     metrics['average_batch'] = re.search(
         r'Average batch: (\d+\.\d+) pkts', output).group(1)
     return metrics
@@ -63,10 +65,15 @@ def run_experiment(total_experiment_iterations: int,
 def dump_metrics_into_csv(all_metrics, pkt_size=ETH_MINIMUM_PKT_SIZE_WITHOUT_CRC):
     csv_file = 'pkt_gen_metrics_{}.csv'.format(pkt_size)
 
-    # Write the metrics to the CSV file
+    # Extract units from the first metric (assuming all metrics have the same units)
+    speed_units = all_metrics[0]['speed_units']
+    throughput_units = all_metrics[0]['throughput_units']
+
+   # Write the metrics to the CSV file
     with open(csv_file, mode='w', newline='') as file:
-        writer = csv.DictWriter(
-            file, fieldnames=['packets_per_sec', 'throughput', 'average_batch'])
+        fieldnames = ['packets_per_sec', 'speed_units',
+                      'throughput', 'throughput_units', 'average_batch']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(all_metrics)
 
@@ -77,17 +84,17 @@ def dump_metrics_into_csv(all_metrics, pkt_size=ETH_MINIMUM_PKT_SIZE_WITHOUT_CRC
     average_batch_data = [float(metrics['average_batch'])
                           for metrics in all_metrics]
 
-    return packets_per_sec_data, throughput_data, average_batch_data
+    return packets_per_sec_data, speed_units, throughput_data, throughput_units, average_batch_data
 
 
-def draw_plots_in_pdf(packets_per_sec_data, throughput_data, average_batch_data):
+def draw_plots_in_pdf(packets_per_sec_data, speed_units, throughput_data, throughput_units, average_batch_data):
     # Create a figure with subplots
     fig, axes = plt.subplots(3, 1, figsize=(10, 15))
     axes[0].plot(packets_per_sec_data, marker='o')
     axes[0].set_ylabel('Packets per Second')
     axes[0].set_title('Packets per Second')
     axes[1].plot(throughput_data, marker='o', color='orange')
-    axes[1].set_ylabel('Throughput (Gbps)')
+    axes[1].set_ylabel(f'Throughput ({throughput_units})')
     axes[1].set_title('Throughput')
     axes[2].plot(average_batch_data, marker='o', color='green')
     axes[2].set_xlabel('Run')
@@ -98,21 +105,23 @@ def draw_plots_in_pdf(packets_per_sec_data, throughput_data, average_batch_data)
 
 
 def main():
-    total_experiment_iterations = 10  # Number of times to run pkt-gen
+    total_experiment_iterations = 30
     processed_pkts_per_iteration = 100
+    packet_size = ETH_MINIMUM_PKT_SIZE_WITHOUT_CRC
     tx_interface = "vale113:01"
     rx_interface = "vale116:01"
 
     all_metrics = run_experiment(total_experiment_iterations=total_experiment_iterations,
                                  tx_interface=tx_interface,
                                  rx_interface=rx_interface,
-                                 processed_pkts_per_iteration=processed_pkts_per_iteration)
+                                 processed_pkts_per_iteration=processed_pkts_per_iteration,
+                                 pkt_size=packet_size)
 
-    packets_per_sec_data, throughput_data, average_batch_data = dump_metrics_into_csv(
+    packets_per_sec_data, speed_units, throughput_data, throughput_units, average_batch_data = dump_metrics_into_csv(
         all_metrics)
 
-    draw_plots_in_pdf(packets_per_sec_data,
-                      throughput_data, average_batch_data)
+    draw_plots_in_pdf(packets_per_sec_data, speed_units,
+                      throughput_data, throughput_units, average_batch_data)
 
 
 if __name__ == "__main__":
